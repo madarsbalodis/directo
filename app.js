df3182a1a71c9e242541b4ac018d9b53bdf8afbb
Madars B <129939477+madarsbalodis@users.noreply.github.com>
2023-06-05 15:55:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-let currentPage = 0;
-let pages = document.querySelectorAll('.page');
 
-function showPage(pageNumber) {
-    pages.forEach((page, index) => {
-        if (index === pageNumber) {
-            page.classList.add('active');
-        } else {
-            page.classList.remove('active');
-        }
-    });
-}
 
-// Use this function to navigate
-function nextPage() {
-    if (currentPage < pages.length - 1) {
-        currentPage++;
-        showPage(currentPage);
-    }
-}
-
-function prevPage() {
-    if (currentPage > 0) {
-        currentPage--;
-        showPage(currentPage);
-    }
+function pageBreak() {
+    alert();
 }
 
