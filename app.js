2dd2672c95ed9a171b2d41dacea14c96f4eca3a5
Madars B <129939477+madarsbalodis@users.noreply.github.com>
2023-06-05 15:26:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-alert();
+let currentPage = 0;
+let pages = document.querySelectorAll('.page');
+
+function showPage(pageNumber) {
+    pages.forEach((page, index) => {
+        if (index === pageNumber) {
+            page.classList.add('active');
+        } else {
+            page.classList.remove('active');
+        }
+    });
+}
+
+// Use this function to navigate
+function nextPage() {
+    if (currentPage < pages.length - 1) {
+        currentPage++;
+        showPage(currentPage);
+    }
+}
+
+function prevPage() {
+    if (currentPage > 0) {
+        currentPage--;
+        showPage(currentPage);
+    }
+}
+
